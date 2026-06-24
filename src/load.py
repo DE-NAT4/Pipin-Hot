8f04c7e628507208f43065ae021150c94e2c8e59
kushima028 <1312011@genstudents.org>
2026-06-24 17:54:51 +0100

    redshift implemented, needs testing

--- src/load.py
+++ src/load.py
@@ -1,5 +1,5 @@
 #import psycopg2
-from utils import db_utils
+#from utils import db_utils
 from psycopg2.extras import execute_values
 
 import logging
@@ -8,22 +8,19 @@ import pandas as pd # only import for autocomplete and hints
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
 
-def load(df: pd.DataFrame, table_name: str):
+def load(df: pd.DataFrame, table_name: str, conn, cur):
     
     try:
-        with db_utils.get_connection() as conn:
-            with conn.cursor() as cur:
+        cols = ", ".join(df.columns)
+        values = [tuple(row) for row in df.itertuples(index=False)]
 
-                cols = ", ".join(df.columns)
-                values = [tuple(row) for row in df.itertuples(index=False)]
+        sql = f"INSERT INTO {table_name} ({cols}) VALUES %s"
 
-                sql = f"INSERT INTO {table_name} ({cols}) VALUES %s"
+        execute_values(cur, sql, values)
 
-                execute_values(cur, sql, values)
+        conn.commit()
 
-                conn.commit()
-
-                LOGGER.info(f"load: loaded {len(df)} rows into '{table_name}'")
+        LOGGER.info(f"load: loaded {len(df)} rows into '{table_name}'")
 
     except Exception as e:
         LOGGER.error("load: load failed. Error: {e}")
--- src/utils/db_utils.py
+++ src/utils/db_utils.py
@@ -1,33 +1,44 @@
 import psycopg2 
-#import os
+import boto3
+import logging
+import json
 
-# from dotenv import load_dotenv
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
-# # LOAD ENVIRONMENT VARIABLES
+# create client for the SSM (Systems Manager) service. Allows API calls to SSM Parameter Store
+ssm_client = boto3.client('ssm')
 
-# load_dotenv()
+# Get the SSM Param from AWS and turn it into JSON
+def get_ssm_param(param_name):
+    LOGGER.info(f'get_ssm_param: getting param_name={param_name}')
+    parameter_details = ssm_client.get_parameter(Name=param_name)
+    redshift_details = json.loads(parameter_details['Parameter']['Value'])
 
-# host_name = os.environ.get("POSTGRES_HOST")
-# port = os.environ.get("DB_PORT")
-# database_name = os.environ.get("POSTGRES_DB")
-# user_name = os.environ.get("POSTGRES_USER")
-# user_password = os.environ.get("POSTGRES_PASSWORD")
+    host = redshift_details['host']
+    user = redshift_details['user']
+    db = redshift_details['database-name']
+    LOGGER.info(f'get_ssm_param: loaded for db={db}, user={user}, host={host}')
+    return redshift_details
 
+def get_connection_and_cursor(redshift_details):
+    try:
+        connection = psycopg2.connect(
+            host=redshift_details['host'],
+            port=redshift_details['port'],
+            dbname=redshift_details['database-name'],
+            user=redshift_details['user'],
+            password=redshift_details['password']
+        )
 
-# CONNECT TO DATABASE
+        cursor = connection.cursor()
 
-# hardcode postgresql creds for testing
+        LOGGER(f'get_connection_and_cursor: connection ready')
+        return connection, cursor
 
-def get_connection():
+    except ConnectionError as e:
+        LOGGER.info(f'get_connection: failed to open connection: {e}')
+        raise e
 
-    connection = psycopg2.connect(
-        host='192.168.0.138',
-        port='5432',
-        dbname='postgres',
-        user='postgres',
-        password='mysecretpassword'
-    )
-
-    return connection
 
 
